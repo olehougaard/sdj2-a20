e6002da9fe506f1023e1fddac4c69d9646dcee64
= <=>
2020-09-14 10:10:20 +0200

    Ready for session 3

---
+++ session3/src/pi/PiService.java
@@ -0,0 +1,23 @@
+package pi;
+
+import java.math.BigDecimal;
+
+import threads.ThreadRunner;
+
+public class PiService {
+    private final PiCache cache;
+
+    public PiService() {
+        this.cache = new PiCache();
+    }
+
+    public BigDecimal computePi(int decimals) {
+        if (!cache.contains(decimals)) {
+            ComputePi computer = new ComputePi(decimals);
+            ThreadRunner.start(computer);
+            // Wait how long?
+            cache.put(decimals, computer.getPiValue());
+        }
+        return cache.getPiValue(decimals);
+    }
+}
